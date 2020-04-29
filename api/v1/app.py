0d217a5704f71c33e4ddfe86c16305e23e77fc94
justinmajetich <justinmajetich@gmail.com>
2020-04-29 16:36:42 +0000

    Return status code with 404 message

--- api/v1/app.py
+++ api/v1/app.py
@@ -5,6 +5,7 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -22,7 +23,9 @@ def teardown(error):
 def not_found(error):
     """Custom 404 error
     """
-    return jsonify({'error': 'Not found'})
+    return jsonify({'error': 'Not found'}), 404
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000', threaded=True)
+    app.run(host=getenv('HBNB_API_HOST'),
+            port=getenv('HBNB_API_PORT'),
+            threaded=True)
